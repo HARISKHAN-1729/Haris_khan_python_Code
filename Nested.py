b7372b73b7263b47d61dda1606c0d08ef77b6a0e
Muhammad Haris Khan <68476240+HARISKHAN-1729@users.noreply.github.com>
2020-09-16 16:54:46 +0500

    Add files via upload

---
+++ Nested.py
@@ -0,0 +1,11 @@
+x=int(input("Enter the value of X ="))
+y=int(input("Enter the value of Y ="))
+if x==y:
+    print("YEAH THEY ARE EQUAL")
+elif x>y:
+    print("X is greater than Y")
+elif x<y:
+    print("X is less than Y")
+else:
+    print("ERROR")
+print("NUMBERS")
